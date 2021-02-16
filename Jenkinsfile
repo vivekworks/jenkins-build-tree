1504af373b053553cb73c12ed6f9b6658aec2369
Vivek T S <vivekts90@gmail.com>
2021-02-16 21:42:16 +0530

    Add Simple Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline {
+
+    agent any
+
+    stages {
+
+        stage("build") {
+            steps {
+                echo 'building the application...'
+            }
+        }
+
+        stage("deploy") {
+            steps {
+                echo 'Deploying the application...'
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/dev/vivekts/jenkins/JenkinsBuildTreeApplication.java
@@ -0,0 +1,13 @@
+package dev.vivekts.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuildTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBuildTreeApplication.class, args);
+	}
+
+}
